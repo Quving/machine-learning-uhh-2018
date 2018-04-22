3aa682be3744f37c870ef030b8a1d4f952e8204c
Vinh Ngu <vinh-ngu@hotmail.com>
2018-04-22 23:40:10 +0200

    Fomat the ouput of task3b.

--- exercises/02/task3.py
+++ exercises/02/task3.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+import numpy as np
+import csv
+
+filename = "housing.csv"
 
 ## TASK 3a ##############################################################
-data = np.genfromtxt('housing.csv', delimiter=',')
+
+# Because np. cannot parse strings, I use csv.reader to get the column names.
+data = np.genfromtxt(filename, delimiter=',')
+with open(filename, "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    header = list(reader)[0]
+
+## TASK 3b ##############################################################
+for head,column in zip(header,list(zip(*data))):
+    column = list(column)
+    column.pop(0)
+    min_val = min(column)
+    max_val = max(column)
+    min_idx = column.index(min_val)
+    max_idx = column.index(max_val)
+    mean = np.mean(column)
+    print(head, "\n\tmin:", min_val , "\t( index:", min_idx,")", "\n\tmax:", max_val, "\t( index:", min_idx,")", "\n\tmean", mean)
+
+## TASK 3c ##############################################################
 
