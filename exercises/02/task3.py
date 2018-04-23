184e40607b11294073deca38f022f578339edadc
Vinh Ngu <vinh-ngu@hotmail.com>
2018-04-23 02:39:20 +0200

    Plot histogram and persist them.

--- exercises/02/task3.py
+++ exercises/02/task3.py
@@ -1,27 +1,47 @@
 #!/usr/bin/python3
 import numpy as np
 import csv
+import math
+import matplotlib.pyplot as plt
+import time
+
+## TASK 3a + 3b +3c #######################################################
 
 filename = "housing.csv"
+bin_total = 50.0
+data_props = {}
+data = np.genfromtxt(filename,
+        skip_header=1,
+        usecols=list(range(0,8)),
+        dtype=None,
+        delimiter=',')
 
-## TASK 3a ##############################################################
 
 # Because np. cannot parse strings, I use csv.reader to get the column names.
-data = np.genfromtxt(filename, delimiter=',')
 with open(filename, "r") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
-    header = list(reader)[0]
+    headers = list(reader)[0]
 
-## TASK 3b ##############################################################
-for head,column in zip(header,list(zip(*data))):
+for header,column in zip(headers,list(zip(*data))):
     column = list(column)
-    column.pop(0)
+
+    # Remove nans.
+    column = [x for x in column if str(x) != 'nan']
     min_val = min(column)
     max_val = max(column)
     min_idx = column.index(min_val)
     max_idx = column.index(max_val)
     mean = np.mean(column)
-    print(head, "\n\tmin:", min_val , "\t( index:", min_idx,")", "\n\tmax:", max_val, "\t( index:", min_idx,")", "\n\tmean", mean)
+    bin_size = np.abs(max_val - min_val) / bin_total
 
-## TASK 3c ##############################################################
+    print(header, "\n\tmin:", min_val , "\t( index:", min_idx,")")
+    print("\tmax:", max_val, "\t( index:", max_idx,")", "\n\tmean", mean)
+    print("\tbin_size", bin_size)
+
+    # Create histograms.
+    a = np.hstack((column, list(np.arange(min_val, max_val, bin_size))))
+    plt.gcf().clear()
+    plt.hist(a, bins='auto')
+    plt.title("Histogram for column '" + header+ "'")
+    plt.savefig(header + ".png", bbox_inches='tight')
 
