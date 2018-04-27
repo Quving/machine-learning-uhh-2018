bed022b0e4b5240afa7c01f385d695a3725f30e7
Vinh Ngu <vinh-ngu@hotmail.com>
2018-04-27 22:21:58 +0200

    Add zip to create zips for submissions.

---
+++ create_zip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p submissions
+for D in `find exercises -maxdepth 1 -mindepth 1 -type d`; do
+    cd $D
+    zip=B$(basename $(pwd))_ML_18_NguReichel_Blatt.zip
+    zip -r $zip . && cd -
+    mv $D/$zip submissions/
+done
