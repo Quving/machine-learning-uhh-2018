2ebd11ff9fcfde2516984015622b1def6125a65a
Vinh Ngu <vinh-ngu@hotmail.com>
2018-04-27 16:15:49 +0200

    Extract plot code to method.

--- exercises/03/vtask1.py
+++ exercises/03/vtask1.py
@@ -11,14 +11,9 @@ desired_bin_numbers = 50
 random.seed(random_seed)
 
 def task1a():
-    uniform_randoms = random.sample(range(0,1000), desired_list_size)
-    max_value = max(uniform_randoms)
-    bin_size = max_value/ float(desired_bin_numbers)
-    a = np.hstack((uniform_randoms, list(np.arange(0, max_value, bin_size))))
-    plt.gcf().clear()
-    plt.hist(a, bins='auto')
-    plt.title("Histogram of uniformed random numbers")
-    plt.savefig("hist_1a.png", bbox_inches='tight')
+    min_rand, max_rand = 0, 1000
+    uniform_randoms = random.sample(range(min_rand, max_rand), desired_list_size)
+    plot_histogram(uniform_randoms, "Histogram of normal random distribution.", "1a.png")
     print("Because the set is generated randomly, the dataset's distribution is balanced. That means every element occurs in an equal density.")
 
 def task1b():
@@ -30,21 +25,14 @@ def task1b():
         plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) *
                 np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
                 linewidth=2, color='r')
-        plt.savefig("hist_1b.png", bbox_inches='tight')
+        plt.savefig("1b.png", bbox_inches='tight')
 
 def task1c():
     n, p  = 20, 0.5
     binom_randoms = np.random.binomial(n, p, desired_list_size)
-    max_value = max(binom_randoms)
-    min_value = min(binom_randoms)
-    bin_size = max_value/ float(desired_bin_numbers)
-    a = np.hstack((binom_randoms, list(np.arange(min_value, max_value, bin_size))))
-    plt.gcf().clear()
-    plt.hist(a, bins='auto')
-    plt.title("Histogram of binomial distribution.")
-    plt.savefig("hist_1c.png", bbox_inches='tight')
+    plot_histogram(binom_randoms, "Histogram of binomial distribution.", "1c.png")
 
-def plot_histogram(raw_list, name):
+def plot_histogram(raw_list, name, save_as):
     max_value = max(raw_list)
     min_value = min(raw_list)
     bin_size = max_value/ float(desired_bin_numbers)
