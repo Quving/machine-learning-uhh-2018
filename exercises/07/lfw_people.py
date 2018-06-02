fa377ba5e26f64ee0c7f592a64c4d5734776ef5a
Vinh Ngu <vinh-ngu@hotmail.com>
2018-06-02 18:10:19 +0200

    Plot n samples of each class.

--- exercises/07/lfw_people.py
+++ exercises/07/lfw_people.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from sklearn.datasets import fetch_lfw_people
 import numpy as np
+import matplotlib.pyplot as plt
 
 class LfwPeople():
     def __init__(self):
@@ -9,7 +10,8 @@ class LfwPeople():
         self.n_features = self.lfw_people.data.shape[1]
         target_names = self.lfw_people.target_names
         self.n_classes = target_names.shape[0]
-
+        self.height, self.width = self.lfw_people.images[0].shape
+        print(self.height, self.width)
         self.value_max, self.value_min = 0, 0
         for image in self.lfw_people.images:
             max_local =  np.amax(image)
@@ -17,14 +19,36 @@ class LfwPeople():
             self.value_max = max_local if max_local > self.value_max else self.value_max
             self.value_min = min_local if min_local > self.value_min else self.value_min
 
-    def print_informations(self):
+    def print_dataset_description(self):
         print("Total dataset size:")
         print("\t", "n_samples: %d" % self.n_samples)
         print("\t", "n_features: %d" % self.n_features)
         print("\t", "n_classes: %d" % self.n_classes)
         print("\t", "value_range: [", self.value_min,",", self.value_max, "]")
 
+    def plot_n_samples_of_each_class(self, n):
+        bag = dict()
+        for class_index, image in zip(self.lfw_people.target, self.lfw_people.images):
+            if class_index in bag:
+                if len(bag[class_index]) < n:
+                    bag[class_index].append(image)
+            else:
+                bag[class_index] = [image]
 
+        col, row = n,len(bag.keys())
+        plt.figure(figsize=(1.8 * col, 2.4 * row))
+        plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
+
+        index = 0
+        for class_index, images in bag.items():
+            for image in images:
+                index += 1
+                plt.subplot(row, col , index)
+                plt.imshow(image.reshape((self.height, self.width)), cmap=plt.cm.gray)
+                plt.title(self.lfw_people.target_names[class_index], size=12)
+            plt.xticks(())
+            plt.yticks(())
+        plt.show()
 
 
 
--- exercises/07/main.py
+++ exercises/07/main.py
@@ -2,7 +2,8 @@
 from lfw_people import LfwPeople
 
 lfwpeople = LfwPeople()
-lfwpeople.print_informations()
+lfwpeople.print_dataset_description()
+lfwpeople.plot_n_samples_of_each_class(n=3)
 
 
 
