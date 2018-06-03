155e28cff5cba7db186caf978aaa4778aad41eac
Vinh Ngu <vinh-ngu@hotmail.com>
2018-06-03 15:51:24 +0200

    Fix warnings, stop plotting, extract script in methods.

--- exercises/07/face_recognition_vinh.py
+++ exercises/07/face_recognition_vinh.py
@@ -4,9 +4,8 @@ from time import time
 import logging
 import matplotlib.pyplot as plt
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.datasets import fetch_lfw_people
-from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.decomposition import RandomizedPCA
@@ -16,51 +15,40 @@ from sklearn.svm import SVC
 print(__doc__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
-lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
 
-n_samples, h, w = lfw_people.images.shape
+def face_recognition(n_components = 150):
+    t0 = time()
+    lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
+    n_samples, h, w = lfw_people.images.shape
+    X = lfw_people.data
+    y = lfw_people.target
+    target_names = lfw_people.target_names
+    n_classes = target_names.shape[0]
 
-X = lfw_people.data
-n_features = X.shape[1]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25)
 
-y = lfw_people.target
-target_names = lfw_people.target_names
-n_classes = target_names.shape[0]
+    pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25)
+    X_train_pca = pca.transform(X_train)
+    X_test_pca = pca.transform(X_test)
 
-n_components = 150
+    param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+                  'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
+    clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+    clf = clf.fit(X_train_pca, y_train)
+    print("done in %0.3fs" % (time() - t0))
 
-t0 = time()
-pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
-print("done in %0.3fs" % (time() - t0))
+    y_pred = clf.predict(X_test_pca)
+    print("done in %0.3fs" % (time() - t0))
 
-eigenfaces = pca.components_.reshape((n_components, h, w))
+    report = classification_report(y_test, y_pred, target_names=target_names)
+    confusion_mat = confusion_matrix(y_test, y_pred, labels=range(n_classes))
 
-X_train_pca = pca.transform(X_train)
-X_test_pca = pca.transform(X_test)
-
-
-
-print("Fitting the classifier to the training set")
-t0 = time()
-param_grid = {'C': [1e3, 5e3, 1e4, 5e4, 1e5],
-              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1], }
-clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
-clf = clf.fit(X_train_pca, y_train)
-print("done in %0.3fs" % (time() - t0))
-print("Best estimator found by grid search:")
-print(clf.best_estimator_)
-
-print("Predicting people's names on the test set")
-t0 = time()
-y_pred = clf.predict(X_test_pca)
-print("done in %0.3fs" % (time() - t0))
-
-print(classification_report(y_test, y_pred, target_names=target_names))
-print(confusion_matrix(y_test, y_pred, labels=range(n_classes)))
-
+    print("done in %0.3fs" % (time() - t0))
+    return {"report": report,
+            "confusion_matrix": confusion_mat,
+            "nc_components": n_components}
 
 def plot_gallery(images, titles, h, w, n_row=3, n_col=4):
     plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
@@ -79,14 +67,5 @@ def title(y_pred, y_test, target_names, i):
     true_name = target_names[y_test[i]].rsplit(' ', 1)[-1]
     return 'predicted: %s\ntrue:      %s' % (pred_name, true_name)
 
-prediction_titles = [title(y_pred, y_test, target_names, i)
-                     for i in range(y_pred.shape[0])]
 
-plot_gallery(X_test, prediction_titles, h, w)
-
-# plot the gallery of the most significative eigenfaces
-
-eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
-plot_gallery(eigenfaces, eigenface_titles, h, w)
-
-plt.show()
+print(face_recognition(n_components=150))
