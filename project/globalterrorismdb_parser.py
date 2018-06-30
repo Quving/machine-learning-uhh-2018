7fb59c3c4ce9994a4fdadb5950d12607d513d549
Vinh <Vinh Ngu>
2018-06-30 14:41:36 +0200

    Plot histogram.

--- project/globalterrorismdb_parser.py
+++ project/globalterrorismdb_parser.py
@@ -2,15 +2,12 @@ import copy
 import json
 import math
 import os
+from multiprocessing import Process
 
-import matplotlib
-
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from scipy.stats import gaussian_kde
-from multiprocessing import Process
 
 
 class GlobalTerrorismDBParser():
@@ -34,17 +31,29 @@ class GlobalTerrorismDBParser():
             with open(os.path.join(self.data_dir, column + ".json"), "w") as f:
                 json.dump(obj=list(self.data.get(column)), fp=f, sort_keys=True, indent=4)
 
+    # Returns from the data-set a column as list.
     def __get_column(self, column):
         return list(self.data.get(column))
 
-    def plot_geographical_heatmap(self):
+    def plot_histogram_for_column(self, column_name, bins, xlabel, ylabel):
+        column = self.__get_column(column_name)
+        plt.gcf().clear()
+        n, bins, patches = plt.hist(column, bins=bins, density=True, facecolor='g', alpha=0.75)
+
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title("Histogram for column '" + column_name + "'")
+        plt.savefig(os.path.join(self.plot_dir, column_name + "_histogram.png"), dpi='figure', bbox_inches='tight')
+
+    def plot_geographical_heatmap(self, filename):
+
+        # Preprocessing
         if not os.path.exists(self.plot_dir):
             os.mkdir(self.plot_dir)
 
         lg = copy.deepcopy(self.__get_column(column="longitude"))
         lat = copy.deepcopy(self.__get_column(column="latitude"))
 
-        ## clean NaN's
         idxs = list()
         for val, (a, b) in enumerate(zip(lg, lat)):
             if math.isnan(a) or math.isnan(b):
@@ -54,32 +63,39 @@ class GlobalTerrorismDBParser():
             del lg[idx - incr]
             del lat[idx - incr]
             incr += 1
-        p1 = Process(target=plot_heatmap_1,
-                     args=[lg, lat, "Geographical heatmap of terrorism attacks", "geo_heatmap_1.png", self.plot_dir])
 
-        p2 = Process(target=plot_heatmap_2,
-                     args=[lg, lat, "Geographical heatmap of terrorism attacks", "geo_heatmap_2.png", self.plot_dir])
+        # Plotting
+        p1 = Process(target=self.plot_heatmap_1,
+                     args=[lg, lat, "Geographical heatmap of terrorism attacks", filename, self.plot_dir,
+                           "Longitude", "Latitude"])
+
         p1.start()
-        p2.start()
-
         p1.join()
-        p2.join()
 
+    def plot_heatmap_1(self, lg, lat, title, filename, plot_dir, xlabel, ylabel):
+        lg_lat = np.vstack([lg, lat])
+        z = gaussian_kde(lg_lat)(lg_lat)
 
-def plot_heatmap_1(lg, lat, title, filename, plot_dir):
-    plt.gcf().clear()
-    lg_lat = np.vstack([lg, lat])
-    z = gaussian_kde(lg_lat)(lg_lat)
-    plt.title(title)
-    plt.scatter(lg, lat, c=z, s=2.5, alpha=1)
-    plt.savefig(os.path.join(plot_dir, filename), bbox_inches='tight')
-    plt.show()
+        plt.gcf().clear()
+        plt.title(title)
+        plt.ylabel(ylabel)
+        plt.xlabel(xlabel)
+        plt.scatter(lg, lat, c=z, s=2.5, alpha=1)
+        plt.savefig(os.path.join(plot_dir, filename), dpi='figure', bbox_inches='tight')
+        plt.show()
 
-
-def plot_heatmap_2(lg, lat, title, filename, plot_dir):
-    plt.gcf().clear()
-    plt.hist2d(lg, lat, (150, 150), cmap=plt.cm.jet, alpha=1)
-    plt.colorbar()
-    plt.title(title)
-    plt.savefig(os.path.join(plot_dir, filename), bbox_inches='tight')
-    plt.show()
+    def plot_heatmap_2(self, lg, lat, title, filename, plot_dir):
+        plt.gcf().clear()
+        plt.hist2d(x=lg,
+                   y=lat,
+                   bins=[50, 50],
+                   cmap=plt.cm.jet,
+                   alpha=1)
+        plt.colorbar()
+        plt.ylabel('latitude')
+        plt.xlabel('longitude')
+        plt.title(title)
+        plt.savefig(os.path.join(plot_dir, filename),
+                    transparent=False,
+                    dpi='figure',
+                    bbox_inches='tight')
--- project/main.py
+++ project/main.py
@@ -2,5 +2,11 @@
 
 from globalterrorismdb_parser import GlobalTerrorismDBParser
 
-gt_parser = GlobalTerrorismDBParser()
-gt_parser.plot_geographical_heatmap()
+if __name__ == "__main__":
+    gt_parser = GlobalTerrorismDBParser()
+
+    # Plot histogram for country
+    gt_parser.plot_histogram_for_column("country", bins=250, xlabel="Country Id", ylabel="Frequency")
+
+    # Plot geographic map
+    gt_parser.plot_geographical_heatmap(filename="geographic_map.png")
