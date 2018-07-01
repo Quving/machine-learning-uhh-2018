c2c21cf966f754a22aed786443b352fe23035534
Vinh <Vinh Ngu>
2018-07-01 20:15:44 +0200

    Implement scatter/heatmap flag for geo-plots.

--- project/computer.py
+++ project/computer.py
@@ -1,10 +1,11 @@
 import copy
 import os
 from multiprocessing import Process
-
+import numpy as np
 from globalterrorismdb_parser import GlobalTerrorismDBParser
 from pyhelpers.cleaner import Cleaner
 from pyhelpers.plotter import Plotter
+from sklearn.neighbors import KNeighborsClassifier
 
 
 class Computer:
@@ -12,7 +13,7 @@ class Computer:
         self.gt_parser = GlobalTerrorismDBParser()
         self.plot_dir = "plots"
 
-    def plot_geographical_heatmap(self, filename):
+    def plot_geographical_heatmap(self, filename, heatmap):
         """
         Plots the longitude and latitude of terrorism attacks to png.
         :param filename:
@@ -23,19 +24,28 @@ class Computer:
         if not os.path.exists(self.plot_dir):
             os.mkdir(self.plot_dir)
 
-        lg = copy.deepcopy(self.gt_parser.get_column(column="longitude"))
-        lat = copy.deepcopy(self.gt_parser.get_column(column="latitude"))
+        x = copy.deepcopy(self.gt_parser.get_column(column="longitude"))
+        y = copy.deepcopy(self.gt_parser.get_column(column="latitude"))
 
-        lg, lat = Cleaner.eliminate_nans(lg, lat)
+        x, y = Cleaner.eliminate_nans(x, y)
 
         # Plotting
         title = "Geographical heatmap of terrorism attacks"
         xlabel = "Longitude"
         ylabel = "Latitude"
-        p1 = Process(target=Plotter.plot_heatmap_1,
-                     args=[lg, lat, title, os.path.join(self.plot_dir, filename), xlabel, ylabel])
-        p1.start()
-        p1.join()
+
+        if heatmap:
+            Plotter.plot_heatmap_1(x, y,
+                                   title=title,
+                                   filename=os.path.join(self.plot_dir, "heatmap_"+filename),
+                                   xlabel=xlabel,
+                                   ylabel=ylabel)
+        else:
+            Plotter.plot_scatter(x, y,
+                                 title=title,
+                                 filename=os.path.join(self.plot_dir, "scatter_" + filename),
+                                 xlabel=xlabel,
+                                 ylabel=ylabel)
 
     def plot_histogram_for_column(self, column_name, bins, xlabel, ylabel, info_threshold,
                                   textbox_x_positional_percentage=0.75,
@@ -58,3 +68,15 @@ class Computer:
                                           ylabel=ylabel, info_threshold=info_threshold,
                                           textbox_x_positional_percentage=textbox_x_positional_percentage,
                                           textbox_drop_percentage=textbox_drop_percentage)
+
+    def compute_knn_on_location(self):
+        lg = copy.deepcopy(self.gt_parser.get_column(column="longitude"))
+        lat = copy.deepcopy(self.gt_parser.get_column(column="latitude"))
+
+        lg, lat = Cleaner.eliminate_nans(lg, lat)
+        X = [[float(i), float(i * 2)] for i in lg]
+        y = [int(i) for i in lat]
+        neigh = KNeighborsClassifier()
+        neigh.fit(X, y)
+
+        return neigh
--- project/main.py
+++ project/main.py
@@ -2,23 +2,32 @@
 
 from computer import Computer
 
-if __name__ == "__main__":
-    computer = Computer()
-
-    # Plot histogram for country
-    computer.plot_histogram_for_column(column_name="country",
-                                       bins=100,
-                                       xlabel="Country Id",
-                                       ylabel="Frequency",
-                                       info_threshold=3)
-
-    # Plot geographic map
-    computer.plot_geographical_heatmap(filename="geographic_map_1.png")
-
+def plot_peaks():
     index = [list(range(0, 10)),
              list(range(140, 150)),
              list(range(90, 100))]
+
     for block in index:
         print(block)
         for country_id in block:
             print("\t", computer.gt_parser.get_country_by_id(country_id))
+
+if __name__ == "__main__":
+    computer = Computer()
+
+    # Plot histogram for country
+    # computer.plot_histogram_for_column(column_name="country",
+    #                                    bins=100,
+    #                                    xlabel="Country Id",
+    #                                    ylabel="Frequency",
+    #                                    info_threshold=3)
+
+    # Plot geographic map (heatmap)
+    computer.plot_geographical_heatmap(filename="geographic_map_1.png", heatmap=False)
+
+
+
+    # Plot peaks
+    # plot_peaks()
+
+    knn = computer.compute_knn_on_location()
