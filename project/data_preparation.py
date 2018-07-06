5eb4bcee142594f84df65fde3771b92e5fc09d7d
Christian Reichel <chrstn.rchl@gmail.com>
2018-07-06 19:55:31 +0200

    Separate dataset logic from general data preparation methods (i.e. make a main file).

--- project/data_preparation.py
+++ project/data_preparation.py
@@ -1,17 +1,11 @@
 import numpy as np
 import pandas as pd
-import math
-import os
-
-# Plotter library
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 # Read the data
 
-def read_data(filename, usecols=[]):
+def read_data(filename, usecols=None):
 
-    df = pd.read_csv(filename, encoding = "ISO-8859-1")
+    df = pd.read_csv(filename, encoding = "ISO-8859-1", usecols=usecols)
     print('Finished reading csv file. Dimensions: {}'.format(df.shape))
 
     return df
@@ -38,118 +32,15 @@ def make_bool_arr(arr, conditions):
 
     return np.array(arr) == conditions
 
-#####
-# The purpose of our classifier is to predict the hostkidoutcome category and a percentage of released persons.
-# Y: hostkidoutcome, npreleased
-# X: extended, iyear, gname_id, nhostkid, ndays, ransom, ransompaid, ishostkid
-#####
-
-### Data filtering
-
-# Read data and exclude cols
-# @Snippet: To exclude: lambda x: x not in ["eventid","imonth","iday", "attacktype2","claims2","claimmode2","claimmode3","gname2"]
-df = read_data('globalterrorismdb_0617dist.csv')
-
-# Filter for only kidnapping data (1st, 2nd or 3rd attack type)
-kidnap_cats = [5,6]
-df = df[df.attacktype1.isin(kidnap_cats) | df.attacktype2.isin(kidnap_cats) | df.attacktype3.isin(kidnap_cats)]
-
-# Filter also broken data from our classes
-df = df[df.hostkidoutcome.notnull()]
-
-# Filter data for NaN nreleased or value -99
-df = df[df.nreleased.notnull()]
-df = df[df.nreleased != -99]
-
-# Filter also data where nhostkid is lower than nreleased
-df = df[df.nhostkid >= df.nreleased ]
-
-### Data augmentation
-
-# Add an ID group for gname to the DataFrame
-df['gname_id'],_ = str_to_index_arr(df['gname'])
-
-# Add a normalisation for how many of the hostage victims survived
-df['nreleased_p'] = np.divide(df.nreleased,df.nhostkid)
-
-### Data plots
-
-# First: a plot about number of kidnapped persons
-sns.set(style="darkgrid", color_codes=True)
-
-g1 = sns.jointplot(
-    'iyear',
-    'nhostkid',
-    data = df,
-    kind = "reg",
-    color = 'r',
-    size = 7,
-    xlim = [1970,2016]
-)
-g1.set_axis_labels('Years','Number of kidnapped victims')
-
-plt.show()
-plt.gcf().clear()
-
-# Outcomes vs percentage of released victims
-
-g2 = sns.violinplot(
-    x = 'hostkidoutcome',
-    y = 'nreleased_p',
-    data = df,
-    hue = 'ransom'
-)
-
-plt.show()
-plt.gcf().clear()
-
-#### Correlation over all columns
-
-corr = df.corr()
-
-# Generate a mask for the upper triangle
-mask = np.zeros_like(corr, dtype=np.bool)
-mask[np.triu_indices_from(mask)] = True
-
-# Set up the matplotlib figure
-f, ax = plt.subplots(figsize=(11, 9))
-
-# Generate a custom diverging colormap
-cmap = sns.diverging_palette(220, 10, as_cmap=True)
-
-# Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(
-    corr,
-    mask=mask,
-    cmap=cmap,
-    vmax=.3,
-    center=0,
-    square=True,
-    linewidths=.5,
-    cbar_kws={"shrink": .5}
-)
-
-
-plt.show()
-plt.gcf().clear()
-
-### Separate set into train, validation, test by assigning each to the preferred class randomly.
-train = df.sample(frac=0.6, replace=True)
-validation = df.sample(frac=0.2, replace=True)
-test = df.sample(frac=0.2, replace=True)
-
 def separate_labels(df, labels):
     '''
     Separates the labels columns from the dataframe and returns the dataframe and the labels
+
+    Returns:
+    - The dataframe of X variables
+    - The dataframe of labels (Y)
+    - An array of each label column separated as array, ordered in the order of labels in labels array
     '''
 
-    return df.drop(labels, axis = 1), df[labels]
+    return df.drop(labels, axis = 1), df[labels], [df[label] for label in labels]
 
-labels = ['hostkidoutcome','nreleased_p']
-train, train_labels = separate_labels(train, labels)
-validation, validation_labels = separate_labels(validation, labels)
-test, test_labels = separate_labels(test, labels)
-
-########
-
-Training
--- project/main.py
+++ project/main.py
@@ -1,34 +1,176 @@
 #!/usr/bin/env python
 
-from computer import Computer
+import numpy as np
+import pandas as pd
 
-def plot_peaks():
-    index = [list(range(0, 10)),
-             list(range(140, 150)),
-             list(range(90, 100))]
+# Plotter library
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-    for block in index:
-        print(block)
-        for country_id in block:
-            print("\t", computer.gt_parser.get_country_by_id(country_id))
+# Own libraries
+import data_preparation as prep
+
+def filter_data(df):
+
+    # Filter for only kidnapping data (1st, 2nd or 3rd attack type)
+    kidnap_cats = [5,6]
+    df = df[df.attacktype1.isin(kidnap_cats) | df.attacktype2.isin(kidnap_cats) | df.attacktype3.isin(kidnap_cats) | df.ishostkid == 1]
+
+    # Drop attacktype columns. They aren't needed anymore
+    df = df.drop(['attacktype1','attacktype2','attacktype3','ishostkid'],axis = 1)
+
+    # Filter also broken data from our classes
+    df = df[df.hostkidoutcome.notnull()]
+
+    # Filter data for NaN nreleased or value -99
+    df = df[df.nreleased.notnull()]
+    df = df[df.nreleased != -99]
+
+    # Filter also data where nhostkid is lower than nreleased
+    df = df[df.nhostkid >= df.nreleased ]
+
+    return df
+
+def augmentate_data(df):
+
+    # Add an ID group for gname to the DataFrame
+    df['gname_id'],_ = prep.str_to_index_arr(df['gname'])
+
+    # Add a normalisation for how many of the hostage victims survived
+    df['nreleased_p'] = np.divide(df.nreleased,df.nhostkid)
+
+    # Add a column of died hostages
+    df['ndied'] = np.subtract(df.nhostkid, df.nreleased)
+
+    # Drop all string columns and keep only numeric ones.
+    df = df._get_numeric_data()
+
+    return df
+
+def handle_NaN_in_data(df):
+
+    from sklearn.preprocessing import Imputer
+    fill_NaN = Imputer(missing_values='NaN', strategy='mean', axis=0)
+    imputed_df = pd.DataFrame(fill_NaN.fit_transform(df))
+    imputed_df.columns = df.columns
+    imputed_df.index = df.index
+
+    df = imputed_df
+
+    return df
+
+def set_NaN_to_value(df, value):
+
+    return df.fillna(value)
+
+def set_unknown_to_NaN(df, unknown_values):
+
+    for unknown_value in unknown_values:
+        df.replace(unknown_value, np.nan)
+
+    return df
+
+def visualize_data(df, path = '', suffix = ''):
+
+    # First: a plot about number of kidnapped persons
+    sns.set(style="darkgrid", color_codes=True)
+
+    g1 = sns.jointplot(
+        'iyear',
+        'nhostkid',
+        data = df,
+        kind = "reg",
+        color = 'r',
+        size = 7,
+        xlim = [1970,2016]
+    )
+    g1.set_axis_labels('Years','Number of kidnapped victims')
+
+    g1.savefig(path+'interaction-iyear_nhostkid'+suffix+'.pdf')
+    # Outcomes vs percentage of released victims
+
+    g2 = sns.violinplot(
+        x = 'hostkidoutcome',
+        y = 'nreleased_p',
+        data = df,
+        hue = 'ransom'
+    )
+
+    g2.figure.savefig(path+'interaction-hostkidoutcome_nreleased_p'+suffix+'.pdf')
+
+    ### Correlation
+
+    corr = df.corr()
+
+    # Generate a mask for the upper triangle
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    # Set up the matplotlib figure
+    f, ax = plt.subplots(figsize=(11, 9))
+
+    # Generate a custom diverging colormap
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    # Draw the heatmap with the mask and correct aspect ratio
+    g3 = sns.heatmap(
+        corr,
+        mask=mask,
+        cmap=cmap,
+        vmax=.3,
+        center=0,
+        square=True,
+        linewidths=.5,
+        cbar_kws={"shrink": .5}
+    )
+
+    g3.figure.savefig(path+'correlation_full'+suffix+'.pdf')
+
+def train(X,Y):
+
+    from sklearn.tree import DecisionTreeClassifier
+
+    model = DecisionTreeClassifier()
+    model.fit(X_train, Y_train)
 
 if __name__ == "__main__":
-    computer = Computer()
 
-    # Plot histogram for country
-    # computer.plot_histogram_for_column(column_name="country",
-    #                                    bins=100,
-    #                                    xlabel="Country Id",
-    #                                    ylabel="Frequency",
-    #                                    info_threshold=3)
+    #####
+    # The purpose of our classifier is to predict the hostkidoutcome category and a percentage of released persons.
+    # Y: hostkidoutcome, npreleased
+    # X: extended, iyear, gname_id, nhostkid, ndays, ransom, ransompaid, ishostkid
+    #####
 
-    # Plot geographic map (heatmap)
-    computer.plot_geographical_heatmap(filename="geomap.png",
-                                       heatmap=False)
+    ### Data filtering
+
+    # Read data and exclude cols
+    # @Snippet: To exclude: lambda x: x not in ["eventid","imonth","iday", "attacktype2","claims2","claimmode2","claimmode3","gname2"]
+    df = prep.read_data('globalterrorismdb_0617dist.csv', usecols=lambda x: x not in ['eventid','imonth','iday','weaptype4','weapsubtype4', 'latitude','longitude'])
+    df = filter_data(df)
+    df = augmentate_data(df)
+
+    # We also have sometimes -99 or -9 as values when things were unknown. We have to replace them as well with NaNs
+    df = set_unknown_to_NaN(df, [-9, -99])
 
 
+    # We have a whole number of columns which contains NaNs for missing data. To overcome those, we simply use the sklearn Imputer to fill the NaNs with the mean values
+    # df = handle_NaN_in_data(df)
 
-    # Plot peaks
-    # plot_peaks()
+    # Plot data
+    visualize_data(df, path="plots_11/")
+    print(df.columns)
 
-    knn = computer.compute_knn_on_location()
+    ### Separate set into train, validation, test by assigning each to the preferred class randomly.
+    train = df.sample(frac=0.6, replace=True)
+    validation = df.sample(frac=0.2, replace=True)
+    test = df.sample(frac=0.2, replace=True)
+
+    labels = ['hostkidoutcome','nreleased_p']
+    X_train, Y_train, Y_train_columns = prep.separate_labels(train, labels)
+    X_validation, Y_validation, Y_validation_columns = prep.separate_labels(validation, labels)
+    X_test, Y_test, Y_test_columns = prep.separate_labels(test, labels)
+
+    ########
+    # Training
+
+    # train(X_train, Y_train)
